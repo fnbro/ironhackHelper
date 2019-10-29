7a0efbe79cce98c801790086e7cbcaf188e47eb0
Rookez <54031130+Rookez@users.noreply.github.com>
2019-10-29 16:35:05 +0100

    add user filter

--- client/src/components/MyBootcamp.tsx
+++ client/src/components/MyBootcamp.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { IState, IFeedbackData, IUserData } from '../state/appState';
+import { IState, IFeedbackData, IUserData, IFilter } from '../state/appState';
 import { ActionType, IAction } from '../framework/IAction';
 import axios from 'axios';
 import { reducerFunctions } from '../reducer/appReducer';
@@ -11,7 +11,7 @@ declare let window: IWindow;
 interface IProps { };
 
 export interface IFilterSurveyAction extends IAction {
-  week: string
+  filter: IFilter
 }
 
 // READ ALL Surveys
@@ -21,9 +21,10 @@ reducerFunctions[ActionType.add_feedbacks_from_server] = function (newState: ISt
   return newState
 }
 
-reducerFunctions[ActionType.filter_week] = function (newState: IState, action: IFilterSurveyAction) {
+reducerFunctions[ActionType.filter_survey] = function (newState: IState, action: IFilterSurveyAction) {
   newState.UI.waitingForResponse = false;
-  newState.BM.surveyFilter = action.week
+  newState.BM.surveyFilter.weekFilter = action.filter.weekFilter;
+  newState.BM.surveyFilter.userFilter = action.filter.userFilter;
   return newState
 }
 
@@ -31,6 +32,8 @@ export default class MyBootcamp extends Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.handleWeekChange = this.handleWeekChange.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+
   }
 
   componentDidMount() {
@@ -48,31 +51,50 @@ export default class MyBootcamp extends Component<IProps, IState> {
 
   render() {
     let weekShow;
-    if (window.CS.getBMState().surveyFilter === "all") {
-      weekShow = window.CS.getBMState().allSurveys.map(survey => <SingleSurvey key={survey._id} survey={survey} />)
+    if (window.CS.getBMState().surveyFilter.weekFilter === "all") {
+      if (window.CS.getBMState().surveyFilter.userFilter === "none") {
+        weekShow = window.CS.getBMState().allSurveys.map(survey => <SingleSurvey key={survey._id} survey={survey} />)
+      }
+      else {
+        weekShow = window.CS.getBMState().allSurveys.filter(survey => survey.submitted_by.username.toString().includes(window.CS.getBMState().surveyFilter.userFilter)).map(survey => <SingleSurvey key={survey._id} survey={survey} />)
+      }
     }
     else {
-      weekShow = window.CS.getBMState().allSurveys.filter(survey => survey.feedback_week.toString() === window.CS.getBMState().surveyFilter.toString()).map(survey => <SingleSurvey key={survey._id} survey={survey} />)
+      if (window.CS.getBMState().surveyFilter.userFilter === "none") {
+        weekShow = window.CS.getBMState().allSurveys.filter(survey => survey.feedback_week.toString() === window.CS.getBMState().surveyFilter.weekFilter.toString()).map(survey => <SingleSurvey key={survey._id} survey={survey} />)
+      }
+      else {
+        weekShow = window.CS.getBMState().allSurveys.filter(survey => survey.feedback_week.toString() === window.CS.getBMState().surveyFilter.weekFilter.toString() && survey.submitted_by.username.toString().includes(window.CS.getBMState().surveyFilter.userFilter)).map(survey => <SingleSurvey key={survey._id} survey={survey} />)
+      }
     }
 
     return (
       <div className="feedbackContainer">
         <div>
-          <select onChange={this.handleWeekChange} id="SurveyFilter" name="type">
-            <option value="all">show me everything...</option>
-            <option value="1" >Week 1</option>
-            <option value="2">Week 2</option>
-            <option value="3">Week 3</option>
-            <option value="4">Week 4</option>
-            <option value="5">Week 5</option>
-            <option value="6">Week 6</option>
-            <option value="7">Week 7</option>
-            <option value="8">Week 8</option>
-            <option value="9">Week 9</option>
-            <option value="10">Week 10</option>
-            <option value="11">Week 11</option>
-            <option value="12">Week 12</option>
-          </select>
+          <h3>Filter:</h3>
+          <div>
+            <label htmlFor="weekFilter">Week: </label>
+            <select onChange={this.handleWeekChange} id="weekFilter" name="weekFilter">
+              <option value="all">show me all weeks</option>
+              <option value="1" >Week 1</option>
+              <option value="2">Week 2</option>
+              <option value="3">Week 3</option>
+              <option value="4">Week 4</option>
+              <option value="5">Week 5</option>
+              <option value="6">Week 6</option>
+              <option value="7">Week 7</option>
+              <option value="8">Week 8</option>
+              <option value="9">Week 9</option>
+              <option value="10">Week 10</option>
+              <option value="11">Week 11</option>
+              <option value="12">Week 12</option>
+            </select>
+          </div>
+          <div>
+            <label htmlFor="userFilter">Username: </label>
+            <input type="text" id="userFilter" onChange={this.handleUserChange} />
+          </div>
+
         </div>
         <h2>All Feedbacks: </h2>
         <div>
@@ -83,12 +105,26 @@ export default class MyBootcamp extends Component<IProps, IState> {
   }
 
   handleWeekChange(event: any) {
-    console.log(event.target.value);
+    let currentFilter: IFilter = window.CS.getBMState().surveyFilter;
     const action: IFilterSurveyAction = {
-      type: ActionType.filter_week,
-      week: event.target.value
+      type: ActionType.filter_survey,
+      filter: {
+        weekFilter: event.target.value,
+        userFilter: currentFilter.userFilter
+      }
     }
     window.CS.clientAction(action);
   }
 
+  handleUserChange(event: any) {
+    let currentFilter: IFilter = window.CS.getBMState().surveyFilter;
+    const action: IFilterSurveyAction = {
+      type: ActionType.filter_survey,
+      filter: {
+        weekFilter: currentFilter.weekFilter,
+        userFilter: event.target.value
+      }
+    }
+    window.CS.clientAction(action);
+  }
 }
--- client/src/state/appState.ts
+++ client/src/state/appState.ts
@@ -90,13 +90,18 @@ export interface IUserData {
     isAdmin?: string;   
 }
 
+export interface IFilter {
+    weekFilter: string,
+    userFilter: string
+}
+
 export interface IBM{
     user:IUser;
     members:IUserData[];
     allNews: INewsData[],
     allSurveys: IFeedbackData[],
     news: INewsData;
-    surveyFilter: string;
+    surveyFilter: IFilter,
     survey: IFeedbackData;
     settings: ISettings;
 }
@@ -145,7 +150,10 @@ export const initial: IState = {
             news_type: INewsType.none,
             created_by: ''
         },
-        surveyFilter: "all",
+        surveyFilter: {
+            weekFilter: "all",
+            userFilter: "none",
+        },
         survey: {
             feedback_week: -1,
             feedback_satisfied: -1,
