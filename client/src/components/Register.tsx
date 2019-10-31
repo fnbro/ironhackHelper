a5fb9b7c83c9b007d19d2391f19fc608643ceabe
Nestler, Florian <nestlerf@mediamarktsaturn.com>
2019-10-31 11:18:18 +0100

    register prob

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -23,13 +23,11 @@ reducerFunctions[ActionType.register_error] = function (newState: IState, action
 }
 
 reducerFunctions[ActionType.update_user] = function (newState: IState, updateAction: IUserAction) {
-    console.log(updateAction.user);
     newState.BM.user = updateAction.user;
     newState.UI.Register.errorMessageRegister = "";
     return newState
 }
 reducerFunctions[ActionType.user_created] = function (newState: IState, updateAction: IUserAction) {
-    console.log(updateAction.user);
     newState.UI.waitingForResponse = false;
     newState.UI.Register.errorMessageRegister = "";
     return newState
@@ -123,7 +121,6 @@ export default class Register extends Component {
         axios.post('/auth/signup', window.CS.getBMState().user)
             .then(res => {
                 const data = res.data;
-                console.log(data);
                 if (data.errorMessage) {
                     const uiAction: IErrorMessage = {
                         type: ActionType.register_error,
@@ -138,7 +135,6 @@ export default class Register extends Component {
                     history.push('/login');
                     window.CS.clientAction(uiAction);
 
-                    console.log(res.data)
                 }
 
             });
