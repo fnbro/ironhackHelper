3859cf5e8558583f14f61cdd27a57faadc093c6c
Nestler, Florian <nestlerf@mediamarktsaturn.com>
2019-10-31 09:17:38 +0100

    show username in navbar

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f13ae87b39969cc237f73fee16ade013.js"
+  "/precache-manifest.a1131bff1267ff4b64727d93308719c7.js"
 );
 
 self.addEventListener('message', (event) => {
